0f55de3e3ac1ccf217b3198555b2f7e8907c8c2a
emmanuel smith <emmanuel.smith@hp.com>
2013-08-09 18:06:53 +0100

    add preliminary test arrray

--- test_test.sh
+++ test_test.sh
@@ -7,9 +7,12 @@ source swiss.sh
 
 main() {
   # TODO(mraxilus): document main
+  # TODO(mraxilus): todo write loop to run through tests
+  tests=(\
+    "name" "pass" "o1" "s1" "o2" "s2" "command" "stdin" \
+  )
   local name="pass on matching expect"
-  local expect="$(swiss::colorize 2 pass):
-  name: \"${name}\""
+  local expect=$(mimic_output "pass" "${name}")
   assert "${name}" "${expect}" "${name}" "valid" "" "echo valid"
   #verify_test "valid" "" "echo invalid" 
 
@@ -23,9 +26,19 @@ main() {
   #verify_test "valid" "invalid" "cat" 0
 }
 
-#mimic_output() {
+mimic_output() {
   # TODO(mraxilus): document mimic_output
-#}
+  # TODO(mraxilus): clean up layout of expect
+  local expect=""
+  if [[ "${1}" == "pass" ]]; then
+    expect+="$(swiss::colorize 2 pass):\n  name: \"${2}\""
+  elif [[ "${1}" == "fail" ]]; then
+    expect+="$(swiss::colorize 1 fail):\n  name: \"${2}\"\n  expect:\n    "
+    expect+="output: \"${3}\"\n    status: ${4}\n  actual:\n    output: "
+    expect+="\"${5}\"\n    status: ${6}"
+  fi
+  echo -e "$expect"
+}
 
 assert() {
   # TODO(mraxilus): document verify_test
