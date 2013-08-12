d70463c2873fdaa2a69ac91ed807cd78be3e7da6
emmanuel smith <emmanuel.smith@hp.com>
2013-08-12 15:36:15 +0100

    add better test case assertion validating

--- test.sh
+++ test.sh
@@ -4,29 +4,64 @@
 
 # TODO(mraxilus): show output only on failures
 # TODO(mraxilus): provide ability to set output verbosity
-# TODO(mraxilus): document assert_equal
-swiss::test::assert_equal() {
-  name="${1}"
-  expect_stdout="${2}"
-  stdin="${3}"
-  command="${4}"
-  expect_status="${5:-0}"
-  actual_stdout=$(echo "${stdin}" | eval "${command}" 2> /dev/null)
-  actual_status=$?
-  if [[ "${expect_stdout}" != "${actual_stdout}" ]] \
-    || [[ "${expect_status}" != "${actual_status}" ]]; then
-    echo "$(swiss::colorize 1 fail):"
-    echo "  name: \"${name}\""
-    echo "  expect:"
-    echo "    output: \"${expect_stdout}\""
-    echo "    status: ${expect_status}"
-    echo "  actual:"
-    echo "    output: \"${actual_stdout}\""
-    echo "    status: ${actual_status}"
-    return 1
-  else
+
+swiss::test::assert() {
+  # detect if input has been provided
+  if read -t 0; then
+    local stdin=$(cat /dev/stdin)
+  fi
+
+  local stderr_file=$(mktemp)
+  local stdout  # hack to get around local elminating exit status
+  stdout=$(echo "${stdin}" | eval "${2}" 2> ${stderr_file})
+  local exit_status=$?
+  local stderr=$(cat ${stderr_file})
+  local result="fail"
+  if [[ "${3}" == "${stdout}" ]] \
+    && [[ "${4:-0}" == "${exit_status}" ]] \
+    && [[ "${5:-${stderr}}" == "${stderr}" ]]; then
+    result="pass"
+  fi
+  swiss::test::_print_result "${result}" "${1}" "${3}" "${4}" "${5}" \
+    "${stdout}" "${exit_status}" "${stderr}"
+
+  # cleanup
+  rm ${stderr_file}
+}
+
+swiss::test::assert_status() {
+  # TODO(mraxilus): impliment assert_status
+  echo "unimplimented"
+}
+
+swiss::test::assert_stderr() {
+  # TODO(mraxilus): impliment assert_stderr
+  echo "unimplimented"
+}
+
+swiss::test::assert_stdout() {
+  # TODO(mraxilus): impliment assert_stdout
+  echo "unimplimented"
+}
+
+swiss::test::_print_result() {
+  # TODO(mraxilus): document _print_result
+  if [[ "${1}" == "pass" ]]; then
     echo "$(swiss::colorize 2 pass):"
-    echo "  name: \"${name}\""
-    return 0
+    echo "  name: \"${2}\""
+  elif [[ "${1}" == "fail" ]]; then
+    echo "$(swiss::colorize 1 fail):"
+    echo "  name: \"${2}\""
+    echo "  expect:"
+    echo "    stdout: \"${3}\""
+    echo "    status: ${4}"
+    echo "    stderr: \"${5}\""
+    echo "  actual:"
+    echo "    stdout: \"${6}\""
+    echo "    status: ${7}"
+    echo "    stderr: \"${8}\""
+  else
+    # TODO(mraxilus): report error
+    echo "unimplimented"
   fi
 }
--- test_test.sh
+++ test_test.sh
@@ -9,21 +9,40 @@ main() {
   # TODO(mraxilus): document main
   # TODO(mraxilus): todo write loop to run through tests
   tests=(\
-    "name" "pass" "o1" "s1" "o2" "s2" "command" "stdin" \
+    # name
+    # result expect       actual       command        stdin
+    "pass on valid stdout" \
+      "pass" "valid" "0" "valid"   "0" "echo valid"   "" \
+    "fail on invalid stdout" \
+      "fail" "valid" "0" "invalid" "0" "echo invalid" "" \
+    "pass on valid stdin" \
+      "pass" "valid" "0" "valid"   "0" "cat"          "valid" \
+    "fail on invalid stdin" \
+      "fail" "valid" "0" "invalid" "0" "cat"          "invalid" \
+    # veriy status
+      #verify_test "valid" "valid"   "cat" 0
+      #verify_test "valid" "valid"   "cat" 1
+      #verify_test "valid" "invalid" "cat" 0
   )
-  local name="pass on matching expect"
-  local expect=$(mimic_output "pass" "${name}")
-  assert "${name}" "${expect}" "${name}" "valid" "" "echo valid"
-  #verify_test "valid" "" "echo invalid" 
 
-  # verify stdin
-  #verify_test "valid" "valid"     "cat" 
-  #verify_test "valid" "invalid" "cat" 
+  if [[ $((${#tests[@]} % 8)) != 0 ]]; then
+    # TODO(mraxilus): provide useful output
+    exit 1
+  fi
 
-  # veriy status
-  #verify_test "valid" "valid"     "cat" 0
-  #verify_test "valid" "valid"     "cat" 1
-  #verify_test "valid" "invalid" "cat" 0
+  for i in $(seq 0 $(((${#tests[@]} / 8) - 1))); do
+    local index=$((i * 8))
+    local name=${tests[$index]}
+    local result=${tests[$(($index + 1))]}
+    local expect_output=${tests[$(($index + 2))]}
+    local expect_status=${tests[$(($index + 3))]}
+    local actual_output=${tests[$(($index + 4))]}
+    local actual_status=${tests[$(($index + 5))]}
+    local command=${tests[$(($index + 6))]}
+    local stdin=${tests[$(($index + 7))]}
+    local assert_output=$(mimic_output "${result}" "${name}" "${expect_output}" "${expect_status}" "${actual_output}" "${actual_status}")
+    assert "${name}" "${assert_output}" "${name}" "${expect_output}" "${stdin}" "${command}"
+  done
 }
 
 mimic_output() {
