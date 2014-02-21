17abb8e4a59193261fd462437e49a0dce23a139c
emmanuel smith <manny@hp.com>
2014-02-21 15:34:19 +0000

    update documentation

--- swiss.sh
+++ swiss.sh
@@ -2,11 +2,13 @@
 # this file is part of swiss.sh which is released under the mit license.
 # go to http://opensource.org/licenses/mit for full details.
 
+# set script options.
 set -o nounset
 set -o errexit
 
+# setup global variables.
 SWISS_ROOT=$(readlink -f ${BASH_SOURCE[0]} | xargs dirname)
-# create array of all submodules excluding tests and this file itself
+# create array of all modules excluding tests and this file itself.
 SWISS_PATHS=($(find ${SWISS_ROOT}/*.sh | sed '/\(swiss\|_test\)\.sh$/d'))
 SWISS_SOURCEE=$(readlink -f ${BASH_SOURCE[1]}) 
 
@@ -15,32 +17,31 @@ swiss::colorize() {
   # globals:
   #   none
   # arguments:
-  #   $1:  color code
-  #   $2:  string
+  #   $1:  color code to use.
+  #   $2:  string to colorize.
   # returns:
-  #   none
-  #
+  #   none.
   # the color codes are as follows:
-  #   0: black
-  #   1: red
-  #   2: green
-  #   3: yellow
-  #   4: blue
-  #   5: magenta
-  #   6: cyan
-  #   7: white
+  #   0: black.
+  #   1: red.
+  #   2: green.
+  #   3: yellow.
+  #   4: blue.
+  #   5: magenta.
+  #   6: cyan.
+  #   7: white.
   echo -e "$(tput setaf ${1})${2}$(tput sgr0)"
 }
 
 swiss::main() {
-  # source all the swiss libraries in the SWISS_ROOT directory
+  # source all the swiss modules in the SWISS_ROOT directory.
   # globals:
-  #   $SWISS_PATHS
-  #   $SWISS_SOURCEE
+  #   SWISS_PATHS:
+  #   SWISS_SOURCEE:
   # arguments:
-  #   none
+  #   none.
   # returns:
-  #   none
+  #   none.
   for swiss_path in "${SWISS_PATHS[@]}"; do
     if [[ "${swiss_path}" != "${SWISS_SOURCEE}" ]]; then
       source "${swiss_path}"
--- test.sh
+++ test.sh
@@ -5,16 +5,16 @@
 swiss::test::assert() {
   # verify that command produces expected output given some input via stdin.
   # globals:
-  #   none
+  #   SWISS_TEST_PASSED: track failed assertion within test.
   # arguments:
-  #   $1: command to execute
-  #   $2: expected string to be produced to stdout
-  #   $3: expected exit status of the command
-  #   $4: expected string to be produced to stderr
+  #   $1: command to execute.
+  #   $2: expected string to be produced to stdout.
+  #   $3: expected exit status of the command.
+  #   $4: expected string to be produced to stderr.
   # returns:
-  #   none
+  #   none.
 
-  # detect if input has been provided
+  # detect if input has been provided.
   local stdin=""
   if read -t 0; then
     stdin=$(cat /dev/stdin)
@@ -22,7 +22,7 @@ swiss::test::assert() {
 
   # retrieve actual output of command.
   local stderr_file=$(mktemp)
-  local stdout  # circumvent local discarding exit status
+  local stdout  # circumvent local command discarding exit status.
   stdout=$(echo "${stdin}" | eval "${1}" 2> ${stderr_file})
   local exit_status=$?
   local stderr=$(cat ${stderr_file})
@@ -43,15 +43,15 @@ swiss::test::assert() {
 }
 
 swiss::test::end_suite() {
-  # finish current suite at print out statistical results.
+  # finish current suite and print out statistical results.
   # globals:
-  #   SWISS_TEST_FAILURES
-  #   SWISS_TEST_PASSES
-  #   SWISS_TEST_TOTAL
+  #   SWISS_TEST_FAILURES: print out total test failures.
+  #   SWISS_TEST_PASSES: print out total test successes.
+  #   SWISS_TEST_TOTAL: print out total amount of tests ran.
   # arguments:
-  #   none
+  #   none.
   # returns:
-  #   none
+  #   none.
   local results="    fail: ${SWISS_TEST_FAILURES}"
   results="${results}"$'\n'"    pass: ${SWISS_TEST_PASSES}"
   results="${results}"$'\n'"    total: ${SWISS_TEST_TOTAL}"
@@ -66,19 +66,24 @@ swiss::test::end_suite() {
 }
 
 swiss::test::end_test() {
-  # 
+  # finish current test, print assertions, and update test statistics.
   # globals:
-  #   SWISS_TEST_DIRECTORY_ORIGINAL:
-  #   SWISS_TEST_DIRECTORY_TEMPORARY:
+  #   SWISS_TEST_ASSERTIONS: print out output of test's assertions.
+  #   SWISS_TEST_PASSED: check if test has failed assertion.
+  #   SWISS_TEST_PASSES: update passed test count.
+  #   SWISS_TEST_FAILURES: update failed test count.
+  #   SWISS_TEST_TOTAL: update total test count.
+  #   SWISS_TEST_DIRECTORY_ORIGINAL: change back to original directory.
+  #   SWISS_TEST_DIRECTORY_TEMPORARY: delete directory to cleanup after test.
   # arguments:
-  #   none
+  #   none.
   # returns:
-  #   none
+  #   none.
   if [[ "${SWISS_TEST_ASSERTIONS}" != "" ]]; then
     echo -e "      assertions:${SWISS_TEST_ASSERTIONS}"
   fi
 
-  # update statistics
+  # update statistics.
   if [[ "${SWISS_TEST_PASSED}" -eq 1 ]]; then
     SWISS_TEST_PASSES=$((${SWISS_TEST_PASSES:-0} + 1))
     echo "      $(swiss::colorize 2 pass): true"
@@ -88,32 +93,32 @@ swiss::test::end_test() {
   fi
   SWISS_TEST_TOTAL=$((${SWISS_TEST_TOTAL:-0} + 1))
 
-  # cleanup after test
+  # cleanup after test.
   cd "${SWISS_TEST_DIRECTORY_ORIGINAL}"
   rm -rf "${SWISS_TEST_DIRECTORY_TEMPORARY}"
 }
 
 swiss::test::run() {
-  # run passed in arguments as command ignoring output and return value.
+  # run passed in arguments as command ignoring output, and return value.
   # globals:
-  #   none
+  #   none.
   # arguments:
-  #   $@: command to execute
+  #   $@: command to execute.
   # returns:
-  #   none
+  #   none.
   eval "$@" &> /dev/null || true
 }
 
 swiss::test::start_suite() {
-  # resets test tracking variables to separate suite statistical information.
+  # start new suite and reset statistical variables for test tracking.
   # globals:
-  #   SWISS_TEST_FAILURES: resets the failed test count 
-  #   SWISS_TEST_PASSES: resets the passed test count
-  #   SWISS_TEST_TOTAL: resets the total test count
+  #   SWISS_TEST_FAILURES: reset failed test count.
+  #   SWISS_TEST_PASSES: reset passed test count.
+  #   SWISS_TEST_TOTAL: reset total test count.
   # arguments:
-  #   $1: name of the suite to start
+  #   $1: name of suite to start.
   # returns:
-  #   none
+  #   none.
   SWISS_TEST_PASSES=0
   SWISS_TEST_FAILURES=0
   SWISS_TEST_TOTAL=0
@@ -123,14 +128,16 @@ swiss::test::start_suite() {
 }
 
 swiss::test::start_test() {
-  # 
+  # start new test and reset relevant variables.
   # globals:
-  #   SWISS_TEST_DIRECTORY_ORIGINAL:
-  #   SWISS_TEST_DIRECTORY_TEMPORARY:
+  #   SWISS_TEST_DIRECTORY_ORIGINAL: store working directory for test end.
+  #   SWISS_TEST_DIRECTORY_TEMPORARY: create and change to temporary directory.
+  #   SWISS_TEST_ASSERTIONS: reset assertion output.
+  #   SWISS_TEST_PASSED: set to passed to verify overall test result.
   # arguments:
-  #   $1: name of test
+  #   $1: name of test to start.
   # returns:
-  #   none
+  #   none.
   SWISS_TEST_DIRECTORY_ORIGINAL="$(pwd)"
   SWISS_TEST_DIRECTORY_TEMPORARY="$(mktemp -d)"
   cd "${SWISS_TEST_DIRECTORY_TEMPORARY}"
@@ -140,36 +147,37 @@ swiss::test::start_test() {
 }
 
 swiss::test::_add_assertion() {
-  # TODO(mraxilus): document _add_assertion
+  # create output from result of assertion and add it to buffer for later use.
   # globals:
-  #   SWISS_TEST_VERBOSE:  if set also be verbose for tests that pass.
+  #   SWISS_TEST_VERBOSE: if set also be verbose for tests that pass.
+  #   SWISS_TEST_ASSERTIONS: buffer used to store all assertion output for test.
   # arguments:
-  #   $1: test result
-  #   $2: command
-  #   $3: expect stdout
-  #   $4: expect status
-  #   $5: expect stderr
-  #   $6: actual stdout
-  #   $7: actual status
-  #   $8: actual stderr
+  #   $1: assertion result (1 if passed, 0 if failed).
+  #   $2: executed command.
+  #   $3: expected stdout.
+  #   $4: expected status.
+  #   $5: expected stderr.
+  #   $6: actual stdout.
+  #   $7: actual status.
+  #   $8: actual stderr.
   # returns:
-  #   none
+  #   none.
 
-  # determine whether to print assertion information
-  if [[ -v SWISS_TEST_VERBOSE || "${1}" == "fail" ]]; then
-    if [[ "${1}" == "pass" ]]; then
+  # determine whether to print assertion information.
+  if [[ -v SWISS_TEST_VERBOSE || "${1}" == "0" ]]; then
+    if [[ "${1}" == "1" ]]; then
       # TODO(mraxilus): translate ascii quotation to unicode in $2.
       SWISS_TEST_ASSERTIONS+="\n        - $(swiss::colorize 2 command): \"${2}\""
-    elif [[ "${1}" == "fail" ]]; then
+    elif [[ "${1}" == "0" ]]; then
       SWISS_TEST_ASSERTIONS+="\n        - $(swiss::colorize 1 command): \"${2}\""
     fi
-    SWISS_TEST_ASSERTIONS="\n          expect:"
-    SWISS_TEST_ASSERTIONS="\n            stdout: \"${3}\""
-    SWISS_TEST_ASSERTIONS="\n            status: ${4}"
-    SWISS_TEST_ASSERTIONS="\n            stderr: \"${5}\""
-    SWISS_TEST_ASSERTIONS="\n          actual:"
-    SWISS_TEST_ASSERTIONS="\n            stdout: \"${6}\""
-    SWISS_TEST_ASSERTIONS="\n            status: ${7}"
-    SWISS_TEST_ASSERTIONS="\n            stderr: \"${8}\""
+    SWISS_TEST_ASSERTIONS+="\n          expect:"
+    SWISS_TEST_ASSERTIONS+="\n            stdout: \"${3}\""
+    SWISS_TEST_ASSERTIONS+="\n            status: ${4}"
+    SWISS_TEST_ASSERTIONS+="\n            stderr: \"${5}\""
+    SWISS_TEST_ASSERTIONS+="\n          actual:"
+    SWISS_TEST_ASSERTIONS+="\n            stdout: \"${6}\""
+    SWISS_TEST_ASSERTIONS+="\n            status: ${7}"
+    SWISS_TEST_ASSERTIONS+="\n            stderr: \"${8}\""
   fi
 }
